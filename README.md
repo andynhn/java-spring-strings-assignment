7ce85edf391e08c2b71bcdbe3052dde000e064ca
Andy N.H. Nguyen <43735217+andynhn@users.noreply.github.com>
2019-01-14 14:04:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-spring-strings-assignment
+Create a Spring Boot application that will respond with some hard coded text for two routes.
---
+++ src/main/java/com/andy/firstproject/StringsprojectApplication.java
@@ -0,0 +1,27 @@
+package com.andy.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 2. Importing Dependencies
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+// 1. Annotation
+@RestController
+public class StringsprojectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StringsprojectApplication.class, args);
+	}
+	// 1. Annotation
+	@RequestMapping("/")
+	// 3. Method that maps to the request route above
+	public String hello() {	// 3
+		return "Hello client! How are you doing?";
+	}
+	@RequestMapping("/random")
+	public String response() {
+		return "Spring Boot is great! So easy to just respond with strings.";
+	}
+
+}
+
